f9ecc9b3e9d6b4b955b6e1ad977d21a64c6782a1
Unknown <timat35@gmail.com>
2018-09-20 06:47:24 +0200

    add anim_png

---
+++ anim_png/anim_png.py
@@ -0,0 +1,46 @@
+import sys
+import os
+from lxml import etree
+import subprocess
+import math
+
+
+
+dpi = 150
+
+
+base = etree.parse('./test.svg')
+base.write('./temp.svg', pretty_print=False)
+
+
+
+root = base.getroot()
+map = root[(len(root)-1)]
+nb_png = len(map)
+
+output = etree.parse('./temp.svg')
+base_temp = output.getroot()
+map_png =  base_temp[(len( base_temp)-1)]
+
+
+for i in range(0,(nb_png)):
+    map_png.remove(map_png[0])
+
+
+for i in range(0,(nb_png)):
+
+    map_png.append(map[0])
+    output.write('./anim_png.svg', pretty_print=False)
+    subprocess.call(['inkscape','--without-gui', '-d' + str(dpi) ,'--export-png=./anim_png/anim_png' + str(i) + '.png', './anim_png.svg'], shell=True)
+    map_png.remove(map_png[0])
+
+
+
+
+
+
+
+
+
+
+
--- delete_object.py
+++ delete_object.py
@@ -3,10 +3,12 @@ import os
 from lxml import etree
 from subprocess import call
 
-dir_update = 'C:/Projects/globocan2018_graph/map by site/asr_abs'
+dir_update = 'C:/Projects/globocan2018_graph/map_by_site/cumrisk_abs'
 
+bool_change = False
 
 for file_sites in os.listdir(dir_update):
+    bool_change = False
     if '.svg' in file_sites:
         path_file = os.path.join(dir_update, file_sites)
         print(path_file)
@@ -14,14 +16,17 @@ for file_sites in os.listdir(dir_update):
         base = etree.parse(path_file)
         root = base.getroot()
         map = root[1]
+
         for child in map:
             if 'd' in child.attrib:
                 if 'M 1329.082031 1337.382813' in child.get("d"):
                     if 'rgb(50.196078%,50.196078%' in child.get("style"):
                         map.remove(child)
+                        bool_change = True
 
 
 
+    if bool_change:
         base.write(path_file, pretty_print=False)
         call(["inkscape", "--without-gui", "--export-pdf="+path_pdf, path_file])
 
