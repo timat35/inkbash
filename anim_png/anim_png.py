ee1d4d52827c7b54034d5fa5d58116d13ba350ed
laversanne <timat35@gmail.com>
2019-02-20 10:58:38 +0100

    fix label improve

--- anim_png/anim_png.py
+++ anim_png/anim_png.py
@@ -4,8 +4,6 @@ from lxml import etree
 import subprocess
 import math
 
-
-
 dpi = 150
 
 
@@ -13,26 +11,26 @@ base = etree.parse('./base_svg.svg')
 base.write('./temp.svg', pretty_print=False)
 
 root = base.getroot()
-map = root[(len(root)-1)]
+map = root[(len(root) - 1)]
 nb_png = len(map)
 
 print(nb_png)
 
 output = etree.parse('./temp.svg')
 base_temp = output.getroot()
-map_png =  base_temp[(len( base_temp)-1)]
+map_png = base_temp[(len(base_temp) - 1)]
 
 
-for i in range(0,(nb_png)):
+for i in range(0, (nb_png)):
 
     map_png.remove(map_png[0])
 
 
-for i in range(0,(nb_png)):
+for i in range(0, (nb_png)):
 
     map_png.append(map[0])
     output.write('./anim_png.svg', pretty_print=False)
-    subprocess.call(['inkscape','--without-gui', '-d' + str(dpi) ,'--export-png=./anim_png/anim_png' + str(i) + '.png', './anim_png.svg'], shell=True)
+    subprocess.call(['inkscape', '--without-gui', '-d' + str(dpi),'--export-png=./anim_png/anim_png'+ str(i) + '.png', './anim_png.svg'], shell=True)
     map_png.remove(map_png[0])
 
 
--- fix_label/fix_label.py
+++ fix_label/fix_label.py
@@ -1,31 +1,28 @@
-import sys
-import copy
 from lxml import etree
-import math
 import subprocess
-import csv
 import re
 
 file_eps = './temp/temp.eps'
-file_svg = file_eps.replace('.eps','.svg')
+file_svg = file_eps.replace('.eps', '.svg')
 file_final = './graph.svg'
 
-subprocess.call(['inkscape','--without-gui', '--export-plain-svg='+file_svg, file_eps], shell=True)
+subprocess.call(['inkscape', '--without-gui', '--export-plain-svg='+file_svg, file_eps], shell=True)
 print("file convert to svg")
 
-base = etree.parse(open(file_svg))
+base = etree.parse(open(file_svg, encoding='utf-8-sig'))
 root = base.getroot()
 
 for elem in root.iter():
     if ('tspan' in elem.tag):
-
         temp_y = re.sub(r"(\d.*?)\s.+$", r"\1", elem.get('y'))
         temp_x = re.sub(r"(\d.*?)\s.+$", r"\1", elem.get('x'))
         elem.set('x', temp_x)
         elem.set('y', temp_y)
+        if ('oba' in elem.text):
+            print(elem.text)
+
 
 base.write('./graph.svg', pretty_print=False)
 
-subprocess.Popen(['inkscape', '-f='+file_final])
+subprocess.Popen(['inkscape', '-f=' + file_final])
 print("look on inkscape")
-
