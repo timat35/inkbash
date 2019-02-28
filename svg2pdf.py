753414a3924926c39247546577fa8941f6d19e9e
laversanne <timat35@gmail.com>
2019-02-28 15:14:59 +0100

    update map year

--- svg2pdf.py
+++ svg2pdf.py
@@ -2,19 +2,15 @@
 import os
 import subprocess
 
-#dir_base = 'C:/Projects/oropharynx/_figs/update'
+# dir_base = 'C:/Projects/oropharynx/_figs/update'
 dir_base = './svg2pdf'
 
 for file_base in os.listdir(dir_base):
     if file_base[-3:] == 'svg':
 
-        file_pdf =  file_base.replace(".svg", ".pdf")
+        file_pdf = file_base.replace(".svg", ".pdf")
         path_svg = os.path.join(dir_base, file_base)
         path_pdf = os.path.join(dir_base, file_pdf)
 
-        subprocess.call(['inkscape','--without-gui', '--export-pdf='+path_pdf, path_svg], shell=True)
+        subprocess.call(['inkscape', '--without-gui', '--export-pdf='+path_pdf, path_svg], shell=True)
         print(file_base + " convert")
-
-
-
-
