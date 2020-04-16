cb3d6f95f2958190226162d0524a33b489bad696
laversanne <timat35@gmail.com>
2020-04-16 18:42:02 +0200

    update

--- gco365/gco365_import.py
+++ gco365/gco365_import.py
@@ -9,12 +9,14 @@ template = 1
 text1 = "upper title"
 text2 = "main title"
 text3 = "subtitle"
-filename = "tweet_test"
+filename = "tweet_test_bar"
+filebase = 'base_bar'
+
 
 file_final = './result/'+ filename + '.svg'
 
 
-base = etree.parse(open('./base/'+ 'base.svg'))
+base = etree.parse(open('./base/'+ filebase +'.svg'))
 root = base.getroot()
 
 # remove name space
@@ -26,26 +28,29 @@ etree.cleanup_namespaces(root)
 root[0].remove( root[0][1])  # drop other css
 
 
-
+# for elem in root.getiterator():
+# 	if elem.tag == 'text':
+# 		print(elem.text)
 # fix for css
 # print(root[0].attrib)
 # root.append(root[0][0]) #copy css ot root
 # 
 
 for child in root:
-	# print(child.tag)
+	print(child.tag)
 	if child.tag == 'image':
 		root.remove(child)
 	if child.tag == 'a':
 		root.remove(child)
 	if child.tag == 'text':
-		# print(child.text)
-		if 'Data source' in child.text:
-			root.remove(child)
-		if 'Graph production' in child.text:
-			root.remove(child)
-		if 'International Agency' in child.text:
-			root.remove(child)
+		print(child.text)
+		if child.text != None:
+			if 'Data source' in child.text:
+				root.remove(child)
+			if 'Graph production' in child.text:
+				root.remove(child)
+			if 'International Agency' in child.text:
+				root.remove(child)
 
 for child in root:
 	# print(child.tag)
@@ -71,6 +76,11 @@ root[1].set("transform", "matrix(0.43424561,0,0,0.43424561,221.08716,113.53036)"
 
 dis = etree.parse(open('./base/gco_template_landscape.svg'))
 root_dis = dis.getroot()
+
+# remove name space
+for elem in root_dis.getiterator():
+	elem.tag = etree.QName(elem).localname
+etree.cleanup_namespaces(root_dis)
 
 # edit text 
 for child in root_dis[3]:
