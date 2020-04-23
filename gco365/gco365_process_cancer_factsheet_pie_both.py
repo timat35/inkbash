c05536c066a11e36a945bffbc925d4cbe06b04ba
laversanne <timat35@gmail.com>
2020-04-23 07:57:04 +0200

    add double pie

--- gco365/gco365_process_cancer_factsheet_pie_both.py
+++ gco365/gco365_process_cancer_factsheet_pie_both.py
@@ -15,13 +15,10 @@ filename = "031_gco365"
 #page of the graph
 page = '1'
 
-# graphic number 1: pie chart new cases
-# graphic number 2: pie chart deaths
-graphic_number = 1
 
 # height of the graph can be edit
 # format is 16:9 (1200*)
-heigth = 1200 
+heigth = 675 
 
 
 file_svg = './result/' + filename+ '.svg'
@@ -47,55 +44,47 @@ etree.cleanup_namespaces(root)
 # regroup element
 
 counter = 0
-
+bool_add = False
 group = etree.Element('g')
 
 
 for child in root[1]:
-	if child.tag == 'path':
 
+
+	if child.tag == 'path':
 		if ('rgb(11.799622%,25.898743%,45.098877%)' in child.get('style')):
 			counter = counter+1
-			if (counter == graphic_number):
+			if (counter == 1):
+				bool_add = True
 				group.append(child)
+			elif (counter == 2):
+				bool_add = True
+				group.append(child)
+			else:
+				bool_add = False
 
-	if (counter == graphic_number):
-		# stop for last graph of the page
-		if len(child)==1:
-			if (child[0].tag == "path"):
-				if ('rgb(4.299927%,50.19989%,71.798706%)' in child[0].get('style')):
-					break
-		# stop for last graph of the page
-		if (child.get('style') != None):
-			if ('rgb(4.299927%,50.19989%,71.798706%)' in child.get('style')):
-				break
 
+	if bool_add:
 		group.append(child)
-	if (counter != graphic_number):
-		if (child.getparent() == root[1]):
-			root[1].remove(child)
+	else:
+		root[1].remove(child)
 
 
 
-for child in root:
-	if (child.get('id') != None):
-		if 'surface' in child.get('id'):
-			root.remove(child)
+
 
 #position of graphic
 
-if graphic_number == 1: 
-	group.set("transform", "matrix(2.9939913,0,0,2.9939913,-155.53437,-91.164833)")
-elif graphic_number == 2: 
-	group.set("transform", "matrix(2.9939913,0,0,2.9939913,-1030.8909,-91.164833)")
+
+group.set("transform", "matrix(2.5645595,0,0,2.5645595,-465.45645,-13.056053)")
 
 root.append(group)
 
 root.set("width", "1200")
-root.set("height", "1200")
+root.set("height", "675")
 
 
-dis = etree.parse(open('./template/gco_template_square.svg'))
+dis = etree.parse(open('./template/gco_template_landscape_pie.svg'))
 root_dis = dis.getroot()
 
 
@@ -111,7 +100,7 @@ for child in root_dis[3]:
 			if elem[0].text == 'title':
 				elem[0].text = title
 
-root_dis[3].set("transform", "matrix(2.6519685,0,0,2.6519685,1200.2178,760.91935)")
+root_dis[3].set("transform", "matrix(4.7146107,0,0,4.7146107,1893.7854,704.13864)")
 
 root.insert(root.index(root[0])+1,root_dis[3])
 
